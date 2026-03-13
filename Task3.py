ac82d9f4aa6cbf8b8a46a082a93cd8f8019853b4
jaskaransingh16 <jaskaransinghbasra4@gmail.com>
2026-03-13 19:29:35 +0530

    added some exception handling in the fast api

--- Task3.py
+++ Task3.py
@@ -1,14 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
-app = FastAPI()
+app = FastAPI(title="Prowiz FastAPI Assignment")
 
 # API 1: Sum of two numbers
 @app.get("/sum")
 def sum_numbers(a: float, b: float):
-    return {"sum": a + b}
+    try:
+        result = a + b
+        return {
+            "number1": a,
+            "number2": b,
+            "sum": result
+        }
+    except Exception:
+        raise HTTPException(status_code=400, detail="Invalid input. Please enter numeric values.")
 
-# API 2: Convert to uppercase
+
+# API 2: Convert lowercase string to uppercase
 @app.get("/uppercase")
-def uppercase(text: str):
-    return {"uppercase": text.upper()}
+def convert_uppercase(text: str):
+    if not text.islower():
+        raise HTTPException(
+            status_code=400,
+            detail="Input must be a lowercase string."
+        )
 
+    return {
+        "original_text": text,
+        "uppercase_text": text.upper()
+    }
